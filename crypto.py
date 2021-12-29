e366ebf7d703190397302bc21b5908462ae0a8e5
Max Candocia <mscandocia@gmail.com>
2021-12-28 21:36:55 -0600

    removed crypto.py

--- crypto.py
+++
@@ -1,9 +0,0 @@
-from argon2 import argon2_hash
-from uuid import uuid4
-import os
-import argparse
-from getpass import getpass
-try:
-        from Cryptodome.Cipher import AES
-except ImportError:
-        from Crypto.Cipher import AES
